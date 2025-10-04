95fd0dad1c3d34205d5ccad6fff059f168859a72
Harshita <85073963+hrshita-kshyp@users.noreply.github.com>
2025-10-04 10:14:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  generateBuildId: async () => {
+    return `build-${Date.now()}`
+  },
+  // Also add this to ensure clean builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+};
 
 export default nextConfig;
